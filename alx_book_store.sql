682b605850025a99f7331a877a048b7c38f3457b
brainskev <ngotho.kelvin@yahoo.com>
2024-10-20 15:40:40 +0300

    Added tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,43 +3,38 @@ CREATE DATABASE IF NOT EXISTS  alx_book_store;
 USE alx_book_store;
 
 
-CREATE TABLE Authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
-    author_name VARCHAR(215) NOT NULL
+CREATE TABLE "Authors" (
+    "author_id" VARCHAR(130) NOT NULL PRIMARY KEY,
+    "author_name" VARCHAR(215) NOT NULL
 );
 
-
-CREATE TABLE Books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+CREATE TABLE "Books" (
+    "book_id" VARCHAR(130) NOT NULL PRIMARY KEY,
+    "title" VARCHAR(130) NOT NULL,
+    "author_id" VARCHAR(130) NOT NULL,
+    "price" DOUBLE NOT NULL,
+    "publication_date" DATE NOT NULL,
+    FOREIGN KEY ("author_id") REFERENCES "Authors"("author_id")
+);
+CREATE TABLE "Customers" (
+    "customer_id" VARCHAR(130) NOT NULL PRIMARY KEY,
+    "customer_name" VARCHAR(215) NOT NULL,
+    "email" VARCHAR(215) NOT NULL,
+    "address" TEXT NOT NULL
 );
 
-
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL UNIQUE,
-    address TEXT NOT NULL
+CREATE TABLE "Orders" (
+    "order_id" VARCHAR(130) NOT NULL PRIMARY KEY,
+    "customer_id" VARCHAR(130) NOT NULL,
+    "order_date" DATE NOT NULL,
+    FOREIGN KEY ("customer_id") REFERENCES "Customers"("customer_id")
 );
 
-
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
-
-
-CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+CREATE TABLE "Order_Details" (
+    "orderdetailid" VARCHAR(130) NOT NULL PRIMARY KEY,
+    "order_id" VARCHAR(130) NOT NULL,
+    "book_id" VARCHAR(130) NOT NULL,
+    "quantity" DOUBLE NOT NULL,
+    FOREIGN KEY ("order_id") REFERENCES "Orders"("order_id"),
+    FOREIGN KEY ("book_id") REFERENCES "Books"("book_id")
 );
