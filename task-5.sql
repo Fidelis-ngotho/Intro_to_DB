aa9798c43b53e538c276398f17170016d6838b6c
fidelis-ngotho <fidelngotho2@gmail.com>
2024-10-23 17:42:21 +0300

    Add MySQLServer.py and SQL files

---
+++ task-5.sql
@@ -0,0 +1,4 @@
+-- Insert a single row into the Customer table without specifying customer_id
+USE alx_book_store;
+INSERT INTO Customes (customer_name, email, address)
+VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
