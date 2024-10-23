4ae4dd45acbec867e1c1a6258643561c5848d2b8
fidelis-ngotho <fidelngotho2@gmail.com>
2024-10-23 12:29:22 +0300

    Fix insertion script by allowing auto-increment for customer_id

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,7 @@
---- Insert a single row into the Customers table
+-- Insert a single row into the Customer table without specifying customer_id
+
+INSERT INTO Customer (customer_name, email, address)
+VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
 INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
