40663db31c3cd1a4a5bffc09e89d381dfafa6fdc
fidelis-ngotho <fidelngotho2@gmail.com>
2024-10-23 17:32:47 +0300

    Add SQL script to create bookstore database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,42 +1,48 @@
-<<<<<<< HEAD
-CREATE DATABASE IF NOT EXISTS  alx_book_store;
+-- Create the database alx_book_store
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
+-- Use the database alx_book_store
 USE alx_book_store;
 
-CREATE TABLE Authors (
-    author_id VARCHAR(130) NOT NULL PRIMARY KEY,
+-- Create the Authors table
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE Books (
-    book_id VARCHAR(130) NOT NULL PRIMARY KEY,
+-- Create the Books table
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
-    author_id VARCHAR(130) NOT NULL,
+    author_id INT,
     price DOUBLE NOT NULL,
-    publication_date DATE NOT NULL,
+    publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-CREATE TABLE Customers (
-    customer_id VARCHAR(130) NOT NULL PRIMARY KEY,
+
+-- Create the Customers table
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT NOT NULL
 );
 
-CREATE TABLE Orders (
-    order_id VARCHAR(130) NOT NULL PRIMARY KEY,
-    customer_id VARCHAR(130) NOT NULL,
+-- Create the Orders table
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_Details (
-    orderdetail_id VARCHAR(130) NOT NULL PRIMARY KEY,
-    order_id VARCHAR(130) NOT NULL,
-    book_id VARCHAR(130) NOT NULL,
+-- Create the Order_Details table
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-=======
->>>>>>> b48524d (add all changes done to Intro-to_DB)
+
