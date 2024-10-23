0beff34b4eea3e720ac9ec0e8490fb369f094437
fidelis-ngotho <fidelngotho2@gmail.com>
2024-10-23 12:38:55 +0300

    Add script to insert a single row into customer table

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 -- Insert a single row into the Customer table without specifying customer_id
 
-INSERT INTO Customer (customer_name, email, address)
+INSERT INTO customer (customer_name, email, address)
 VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
