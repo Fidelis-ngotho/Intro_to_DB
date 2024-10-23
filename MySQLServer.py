5059642c0a45816e19735219bf7bdbc68e5c6d03
fidelis-ngotho <fidelngotho2@gmail.com>
2024-10-23 17:51:52 +0300

    add MySQLServer.py and its changes

--- MySQLServer.py
+++ MySQLServer.py
@@ -23,7 +23,7 @@ def create_database():
             # Print success message
             print("Database 'alx_book_store' created successfully!")
 
-    except mysql.connector. Error as err:
+    except mysql.connector.Error as err:
         # Print error message if there's an issue
         print(f"Error: '{err}'")
 
